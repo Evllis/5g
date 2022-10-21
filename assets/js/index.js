1cf6abbd5b190840d2403a41fa9cab770f34eaa9
Alliestrasza <evllis.jolia@gmail.com>
2022-10-21 18:03:41 +0800

    first ajax

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,64 @@
 (function($, win) {
 
-    console.log(111111111);
+    var Bond = function() {
+        this.ajaxUrl = {
+            packageList: 'https://www.myafq.com/advanceMargin/marginPackage/packageList'
+        }
+    }
+
+    $.extend(Bond.prototype, {
+
+        /**
+         * 顶部导航返回按钮
+         */
+        initNavBack: function() {
+            $('.nav-tool__icon').on('click', function() {
+                win.location.href = 'https://evllis.github.io/5g';
+            });
+        },
+
+        initSwiper: function() {
+            var mySwiper = new Swiper('.swiper', {
+                initialSlide: 0,
+                loop: true,
+                slidesPerView: 'auto',
+                centeredSlides: true,
+                spaceBetween: 0,
+                pagination: {
+                    el: '.swiper-pagination'
+                },
+            })
+        },
+
+        getPackageList: function() {
+            $.ajax({
+                url: this.ajaxUrl.packageList,
+                type: 'POST',
+                data: {},
+                dataType: 'json',
+                xhrFields: {
+                    withCredentials: true
+                },
+                crossDomain: true,
+                success: function(res) {
+                    console.log(11111111, res);
+                },
+                error: function(err) {
+                    console.log(222222222, err);
+                }
+            });
+        },
+
+        init: function() {
+            this.initNavBack();
+            this.initSwiper();
+            this.getPackageList();
+        }
+
+    });
+
+    var bond = new Bond();
+    bond.init();
+    win.bond = bond;
 
 })(jQuery, window);
